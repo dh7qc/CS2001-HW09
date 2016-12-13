0e9461bc9a4b0d64d7994e664f0c3d2ff7129c92
Dennis Hahn <dh7qc@mst.edu>
2016-12-12 23:02:08 -0600

    Program works the same as the reference, needs format checked and comments.

--- Main.elm
+++ Main.elm
@@ -70,20 +70,66 @@ Returns `Just String` if there was a problem. The corresponding
 -}
 validString : String -> Maybe String
 validString x =
-    Nothing
+    if (Set.diff ((x |> String.toList) |> Set.fromList) validChars) |> Set.isEmpty 
+    then 
+      Nothing 
+    
+    else  
+       Just (
+         Set.diff (x |> String.toList  |> Set.fromList ) validChars
+         |> Set.toList  
+         |> List.map String.fromChar 
+         |> String.join ", "
+       )
 
+{- Takes a string, runs it through validString, 
+   turns the Maybe String into a String and returns it.
+-}
+validStringHelper : String -> String
+validStringHelper x = 
+    case validString x of
+      Just value -> value
+      Nothing -> ""
+      
+{-| Translate a single word `String` as necessary.
+
+Takes a string containing one word, translates it 
+if necessary, otherwise returns the original string.
+-}     
+replace : String -> String
+replace x = 
+    if x == "JERK" 
+      then "JOIK"
+    else if x == "YOU" 
+      then "YOUS"
+    else if x == "YOUR" || x == "YOURE" 
+      then "YER"
+    else if String.right 3 x == "ING" 
+      then String.left (String.length x - 3) x ++ "IN"
+    else if String.left 2 x == "TH" 
+      then "D" ++ String.right(String.length x - 2) x
+    else
+      x
 
 {-| Attempt to translate a `String` to Meowth speak.
 
 Returns `Ok String` with the translated string if everything went
 well.
 
+
 Returns `Err String` when the input string is invalid.
 -}
 meowthify : String -> Result String String
-meowthify s =
-    Ok s
-
+meowthify s = 
+      if validString s == Nothing
+      then 
+        Ok (String.join " " (List.map replace (s |> String.words)))
+        
+      else if (s |> validStringHelper |> String.length ) == 1 then
+        Err ("String contains an illegal character: " ++ validStringHelper s)
+      else
+        Err ("String contains illegal characters: " ++ validStringHelper s)
+            
 
 {-| Insert the (uncertain)phrase "YANNO WHAT I MEAN" into a `String`
 at a given index. The phrase is always inserted between words.
@@ -94,22 +140,59 @@ is inserted at the beginning or the end of the `String`.
 -}
 addUncertainty : Int -> String -> String
 addUncertainty i string =
-    String.join " " <| List.concat [ [ "YANNO WHAT I MEAN" ], [ string ] ]
-
+    if i < 0 then 
+      String.join " " <| List.concat [ [ "YANNO WHAT I MEAN" ], [ string ] ]
+    else if i > (List.length <|String.words <| string) then
+      String.join " " <| List.concat [ [ string ] , [ "YANNO WHAT I MEAN" ]]
+    else  
+      let 
+        a = List.partition (\x -> (Tuple.first x) < i) (List.indexedMap (,) (string |> String.words))
+             |> Tuple.first
+             |> List.unzip
+             |> Tuple.second
+        b = List.partition (\x -> (Tuple.first x) < i) (List.indexedMap (,) (string |> String.words))
+             |> Tuple.second
+             |> List.unzip
+             |> Tuple.second
+      in
+        String.join " " <| List.concat [a, [ "YANNO WHAT I MEAN" ], b]
+          
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NewContent content ->
-            ( model, Cmd.none )
+            let
+                m =  
+                   {
+                    translated = case content |> String.toUpper |> meowthify of
+                         Ok str -> str
+                         Err errorMsg -> ""
+                    ,       
+                    errorMsg = case content |> String.toUpper |> meowthify of
+                         Ok str -> ""
+                         Err errorMsg -> errorMsg
+                    }
+            in
+              ( m, Cmd.none )  
 
         AddUncertainty ->
-            ( model, Cmd.none )
+            let
+              c = Random.generate RandomIndex (Random.int 0 (String.length model.translated))
+                
+            in     
+              ( model, c )
 
         RandomIndex i ->
-            ( model, Cmd.none )
-
-
+            let
+                m =  
+                   {
+                    translated = addUncertainty i model.translated
+                    ,       
+                    errorMsg = model.errorMsg
+                    }
+            in
+              ( m, Cmd.none )  
 
 -- VIEW
 
